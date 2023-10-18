64fba75e20c4db89a1afa2fb8126af4819536203
konjikun <shintaniallen@gmail.com>
2023-10-18 21:28:44 +0900

    deploy site

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -10,15 +10,15 @@ const config = {
   favicon: '/img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-test-site.com',
+  url: 'https://edison-js-document.vercel.app/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'Edison', // Usually your GitHub org/user name.
+  projectName: 'Edison', // Usually your repo name.
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
@@ -69,6 +69,7 @@ const config = {
           src: '/img/edison.png',
         },
         
+
         items: [
           {
             href: 'https://github.com/AllenShintani/Edison',
